4b28f5aa67eb5762b8b85d2bec14967c7d63d288
DetermiNemo <DetermiNemo@outlook.com>
2026-01-10 23:55:26 +0800

    lab04

--- lab04/tests/TriangleTest.java
+++ lab04/tests/TriangleTest.java
@@ -14,13 +14,13 @@ public abstract class TriangleTest {
     @Test
     public void sidesFormTest() {
         Triangle t = getNewTriangle();
-        assertThat(t.sidesFormTriangle(2, 2, 3)).isTrue();
-        assertThat(t.sidesFormTriangle(1, 2, 3)).isFalse();
-        assertThat(t.sidesFormTriangle(-1, 2, 3)).isFalse();
-        assertThat(t.sidesFormTriangle(0, 2, 3)).isFalse();
-        assertThat(t.sidesFormTriangle(1000, 2, 3)).isFalse();
-        assertThat(t.sidesFormTriangle(1, 2000, 3)).isFalse();
-        assertThat(t.sidesFormTriangle(1, 2, 3000)).isFalse();
+        assertWithMessage("Wrong").that(t.sidesFormTriangle(2, 2, 3)).isTrue();
+        assertWithMessage("Wrong").that(t.sidesFormTriangle(1, 2, 3)).isFalse();
+        assertWithMessage("Wrong").that(t.sidesFormTriangle(-1, 2, 3)).isFalse();
+        assertWithMessage("Wrong").that(t.sidesFormTriangle(0, 2, 3)).isFalse();
+        assertWithMessage("Wrong").that(t.sidesFormTriangle(1000, 2, 3)).isFalse();
+        assertWithMessage("Wrong").that(t.sidesFormTriangle(1, 2000, 3)).isFalse();
+        assertWithMessage("Wrong").that(t.sidesFormTriangle(1, 2, 3000)).isFalse();
         // remember that you'll have to call on Triangle methods like
         // t.functionName(arguments), where t is a Triangle object
     }
@@ -28,24 +28,24 @@ public abstract class TriangleTest {
     @Test
     public void pointsFormTest() {
         Triangle t = getNewTriangle();
-        assertThat(t.pointsFormTriangle(0, 0, 3, 0, 0, 4)).isTrue();
-        assertThat(t.pointsFormTriangle(2, 3, 3, 9, 12, 4)).isTrue();
-        assertThat(t.pointsFormTriangle(2, 3, 2, 9, 2, 4)).isFalse();
+        assertWithMessage("Wrong").that(t.pointsFormTriangle(0, 0, 3, 0, 0, 4)).isTrue();
+        assertWithMessage("Wrong").that(t.pointsFormTriangle(2, 3, 3, 9, 12, 4)).isTrue();
+        assertWithMessage("Wrong").that(t.pointsFormTriangle(2, 3, 2, 9, 2, 4)).isFalse();
     }
 
     @Test
     public void triangleTypeTest() {
         Triangle t = getNewTriangle();
-        assertThat(t.triangleType(1, 1, 1)).isEqualTo("Equilateral");
-        assertThat(t.triangleType(1, 2, 2)).isEqualTo("Isosceles");
-        assertThat(t.triangleType(2, 4, 3)).isEqualTo("Scalene");
+        assertWithMessage("Wrong").that(t.triangleType(1, 1, 1)).isEqualTo("Equilateral");
+        assertWithMessage("Wrong").that(t.triangleType(1, 2, 2)).isEqualTo("Isosceles");
+        assertWithMessage("Wrong").that(t.triangleType(2, 4, 3)).isEqualTo("Scalene");
     }
 
     @Test
     public void squaredHypotenuseTest() {
         Triangle t = getNewTriangle();
-        assertThat(t.squaredHypotenuse(3, 4)).isEqualTo(25);
-        assertThat(t.squaredHypotenuse(5, 4)).isEqualTo(41);
+        assertWithMessage("Wrong").that(t.squaredHypotenuse(3, 4)).isEqualTo(25);
+        assertWithMessage("Wrong").that(t.squaredHypotenuse(5, 4)).isEqualTo(41);
     }
 
 
